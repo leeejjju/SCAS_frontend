d7e175247f399b29a60a4a38442b195add9cbccf
leeejjju <leeejjju@gmail.com>
2025-09-24 18:36:02 +0900

    basic src code added (not wrote yet...)

--- scas-frontend/src/App.js
+++ scas-frontend/src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './assets/images/SCAS_LOGO.jpg';
 import './App.css';
 
 function App() {
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          야옹이... 
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://forms.gle/wUXgP5nB2Xg2rjwx9"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          출결 기록하기
         </a>
       </header>
     </div>
